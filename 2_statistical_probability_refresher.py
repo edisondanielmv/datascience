6afd5bd71baa9b051fc6122ce5b49cacd7772f41
Edison <edison.daniel.mv@gmail.com>
2020-10-25 22:19:29 -0500

    fifth commit

--- 2_statistical_probability_refresher.py
+++ 2_statistical_probability_refresher.py
@@ -1,5 +1,7 @@
 # -------GENERACION DE UNA FUNCION NORMAL DE LA DISTRIBUCION DE SALARIOS -----------
 
+# -------------MEAN------------
+
 import numpy as np
 incomes= np.random.normal(27000,15000, 10000)
 print(np.mean(incomes))
@@ -16,7 +18,30 @@ print(np.median(incomes))
 
 # ----------- INCLUIR UN OUTLIER--------
 
-incomes=np.append(incomes,[100000000])
+incomes=np.append(incomes,[1000000000])
+
+
+
+# ------------- MEDIAN---------------------
 
 print(np.median(incomes))   #  SE VERIFICA QUE LA MEDIANA NOCAMBIA MUCHOA PESAR DE QUE SE A;ADIO UN  VALOR EXTREMO
 
+print(np.mean(incomes))
+
+# -----------MODE -----------------
+
+ages =np.random.randint(18, high=90, size=500)
+print(ages)
+
+from scipy import stats
+print(stats.mode(ages))
+
+
+# ------------------PRACTICE ------------------
+incomes=np.random.normal(100.0,90.0,10000)
+plt.hist(incomes, 50)
+plt.show()
+
+print(np.mean(incomes))
+from scipy import stats
+print(stats.mode(incomes))
